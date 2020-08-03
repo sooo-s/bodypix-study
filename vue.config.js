b2690b3316a109ff310e72a4dd9c7bd2684642f1
sooo <mail@sooo.space>
2020-08-04 08:02:29 +0900

    deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bodypix-study/'
+    : '/',
   configureWebpack: {
     resolve: {
         alias: {
